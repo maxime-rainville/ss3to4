7de5c148bb4b3b83ebba2572b9cb91f36f7db756
Maxime Rainville <maxime@silverstripe.com>
2018-10-10 17:21:54 +1300

    Set up a branch to test imagined-classy-file

--- _ss_environment.php
+++ _ss_environment.php
@@ -6,13 +6,13 @@ define('SS_ENVIRONMENT_TYPE', 'dev');
 define('SS_DATABASE_SERVER', 'localhost');
 define('SS_DATABASE_NAME', 'ss3to4');
 
-//define('SS_DATABASE_CLASS', 'MySQLPDODatabase');
-//define('SS_DATABASE_USERNAME', 'root');
-//define('SS_DATABASE_PASSWORD', '');
+define('SS_DATABASE_CLASS', 'MySQLPDODatabase');
+define('SS_DATABASE_USERNAME', 'root');
+define('SS_DATABASE_PASSWORD', '');
 
-define('SS_DATABASE_CLASS', 'PostgreSQLDatabase');
-define('SS_DATABASE_USERNAME', 'silverstripe');
-define('SS_DATABASE_PASSWORD', 'admin');
+//define('SS_DATABASE_CLASS', 'PostgreSQLDatabase');
+//define('SS_DATABASE_USERNAME', 'silverstripe');
+//define('SS_DATABASE_PASSWORD', 'admin');
 
 /* Configure a default username and password to access the CMS on all sites in this environment. */
 define('SS_DEFAULT_ADMIN_USERNAME', 'admin');
